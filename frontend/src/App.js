b8d9d39e2ffa0a155031837c0ceb1a2d0bebd57e
Prashant Bhat <prashantbhat84@gmail.com>
2020-10-01 21:41:15 +0530

    Basic Header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { Container } from 'react-bootstrap'
 import './App.css';
 
 function App() {
   return (
     <div>
       <Header />
-      Welcome to proshop
+      <main class="py-3">
+        <Container>
+
+          <h1> Welcome to proshop</h1>
+        </Container>
+
+      </main>
+
+
       <Footer />
     </div>
   );
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
     return (
-        <div>
-            footer
-        </div>
+        <Container>
+            <Row>
+                <Col className="text-center py-3">
+                    CopyRight &copy; ProShop
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
