676e1ad9c4a4ef8fb43d8756d6f74366dd0a6229
Prashant Bhat <prashantbhat84@gmail.com>
2020-10-04 22:37:08 +0530

    refactoring get product by id

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -16,10 +16,11 @@ router.get("/:id", asyncHandler(async (req, res) => {
 
 
     const product = await Product.findById(req.params.id);
-    if (!product) {
-        return res.status(404).json({ message: "Product not found" })
+    if (product) {
+        return res.status(200).json(product)
     }
-    res.status(200).json(product)
+    res.status(404)
+    throw new Error("Product not found")
 
 }))
 
