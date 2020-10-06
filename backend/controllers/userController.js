99671abf7d35f6b40939b01ce6d2609eb8cc0117
Prashant Bhat <prashantbhat84@gmail.com>
2020-10-06 19:33:41 +0530

    backend authentication complete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,7 +21,48 @@ export const authUser = asyncHandler(async (req, res, next) => {
         res.status(401)
         throw new Error('Invalid email or password')
     }
+});
+//@desc Get Logged in users profile
+//@method GET
+//@route /api/users/profile
+//@access private
+export const authUserProfile = asyncHandler(async (req, res, next) => {
+
+    const user = req.user;
+    if (!user) {
+
+        res.status(404)
+        throw new Error("Not Found")
+    }
+    res.json(user)
 
 
+});
+//@desc Register a new user
+//@method POST 
+//@route /api/users/profile
+//@access private
+export const registerUser = asyncHandler(async (req, res, next) => {
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+    if (userExists) {
+        res.status(400)
+        throw new Error("User already exists")
+    }
+    const user = await User.create({ name, email, password });
+    if (user) {
+        res.status(201).json({
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        })
+    }
+    else {
+        res.status(400)
+        throw new Error("Invalid User data")
+    }
 
 });
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { authUser } from '../controllers/userController.js'
+import { authUser, authUserProfile, registerUser } from '../controllers/userController.js'
+import { protect } from '../middleware/authMiddleware.js'
 
 router.post("/login", authUser)
+router.get("/profile", protect, authUserProfile)
+router.post("/", registerUser);
 
 export default router;
